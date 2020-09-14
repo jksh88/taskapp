929672899e78fd2f91de99cd5a4e8fb5fd7ca484
Joohoon Kim <joohoonkim@joohoons-mbp.attlocal.net>
2020-09-14 14:23:13 -0400

    inserted documents

--- mongodb.js
+++ mongodb.js
@@ -11,9 +11,48 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (err, client) => {
   console.log("connected succsel");
 
   const collection = db.collection("users");
-  collection.insertOne({
-    name: "Andrew",
-    age: 33,
-    country: "US",
-  });
+  //   collection.insertOne(
+  //     {
+  //       name: "Andrew",
+  //       age: 33,
+  //       country: "US",
+  //     },
+  //     (error, result) => {
+  //       if (error) {
+  //         console.log("failure in inserting document");
+  //       }
+  //       console.log(result.ops);
+  //     }
+  //   );
+  collection.insertMany(
+    [
+      {
+        name: "Jay",
+        age: 32,
+        country: "Korea",
+      },
+      { name: "Jonas", age: 35, country: "Germany" },
+      { name: "Andre", age: 34, country: "Sweden" },
+    ],
+    (error, result) => {
+      if (error) {
+        console.log(error);
+      }
+      console.log(result.ops);
+    }
+  );
+
+  const inventory = db.collection("inventory");
+  inventory.insertMany(
+    [
+      { paper: 30, rubber: 20, powder: 100 },
+      { soap: "dial", shampoo: "head & shoulders" },
+    ],
+    (error, result) => {
+      if (error) {
+        console.log(error.message);
+      }
+      console.log(result.insertedCount, result.ops);
+    }
+  );
 });
