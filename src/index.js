2b01683d67fda27401413cdcd6a1aabfd1d1d041
Joohoon Kim <joohoonkim@joohoons-mbp.attlocal.net>
2020-09-22 23:20:58 -0400

    added endpoints to handle requests for users and tasks

--- src/index.js
+++ src/index.js
@@ -3,9 +3,46 @@ const app = express();
 require("./db/mongoose");
 const User = require("./models/user");
 const Task = require("./models/task");
+const { Model } = require("mongoose");
 const port = process.env.PORT || 3000;
 
 app.use("/", express.json());
+
+app.get("/tasks", (req, res) => {
+  Task.find({})
+    .then((tasks) => res.status(200).send(tasks))
+    .catch((err) => res.status(500).send(err));
+});
+
+app.get("/tasks/:id", (req, res) => {
+  Task.findOne({ _id: { $eq: req.params.id } })
+    .then((task) => {
+      if (!task) {
+        return res
+          .status(404)
+          .send("we couldn't find what you were looking for");
+      }
+      res.send(task);
+    })
+    .catch((error) => res.send(error));
+});
+
+app.get("/users", (req, res) => {
+  const allUsers = User.find({});
+  allUsers.then((result) => res.send(result)).catch((err) => res.send(err));
+});
+
+app.get("/users/:id", (req, res) => {
+  const _id = req.params.id;
+  User.findById(_id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send();
+      }
+      res.status(200).send(user);
+    })
+    .catch((err) => res.status(500).send(err));
+});
 
 app.post("/tasks", (req, res) => {
   const task = new Task(req.body);
@@ -13,7 +50,7 @@ app.post("/tasks", (req, res) => {
     .save()
     .then(() => res.status(200).send(task))
     .catch((err) => {
-      res.status(400).send(err);
+      res.status(500).send(err);
     });
 });
 
@@ -23,7 +60,7 @@ app.post("/users", (req, res) => {
     .save()
     .then(() => res.status(200).send(user))
     .catch((err) => {
-      res.status(400);
+      res.status(500);
       res.send(err);
     });
 });
